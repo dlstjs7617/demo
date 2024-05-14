61f4b79bb4884103bbafcd621bc263772728decf
dlstjs7617 <dlstjs7617@gmail.com>
2024-05-14 14:12:46 +0900

    test3

--- src/routes/Header.js
+++ src/routes/Header.js
@@ -1,10 +1,26 @@
+import { Heading, Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react/dist";
 import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+        <>
+        <Heading>
+            검색 서비스
+        </Heading>
+        <Breadcrumb>
+            <BreadcrumbItem>
+                <BreadcrumbLink href='/demo'>Home</BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem>
+                <BreadcrumbLink href='/demo/video'>Video</BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem isCurrentPage>
+                <BreadcrumbLink href='/demo'>Book</BreadcrumbLink>
+            </BreadcrumbItem>
+        </Breadcrumb>
+        </>
     );
 };
 
